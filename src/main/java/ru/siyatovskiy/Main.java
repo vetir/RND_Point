4c0e76c2add6581a7a64d65d8b1b779c3839bade
vetir@mail.ru <vetir@mail.ru>
2022-01-27 18:03:34 +0300

    Siyatovskiy Yuriy, solution task_4

---
+++ src/main/java/ru/siyatovskiy/Main.java
@@ -0,0 +1,19 @@
+package ru.siyatovskiy;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] masRow = new String[] {
+                "Winter Winter Winter",
+                "Spring Spring Spring Spring",
+                "Summer",
+                "Autumn",
+                "Spring Spring Spring Spring",
+                "Winter Winter Winter",
+                "Spring Spring Spring Spring",
+                "Spring",
+                "Summer",
+        };
+    SolutionTask_4 solutionTask_4 = new SolutionTask_4();
+    solutionTask_4.stringArrayParsing(masRow);
+    }
+}
---
+++ src/main/java/ru/siyatovskiy/SolutionTask_4.java
@@ -0,0 +1,59 @@
+package ru.siyatovskiy;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 4.	Напишите приложение на Java\Groovy, которое решает следующую задачу:
+ *      Имеется массив строк.
+ *      Необходимо определить строку, которая встречается чаще всего и вывести количество слов в данной строке.
+ */
+
+public class SolutionTask_4 {
+    /**
+     * Решение в "лоб" через циклы
+     */
+//        int count = 0;
+//        int maxCount = 0;
+//        String SamayaChastoPovtStroka = "";
+//        for (int i = 0; i < masRow.length; i++) {
+//            if (count > maxCount) {
+//                maxCount = count;
+//                count = 0;
+//                SamayaChastoPovtStroka = masRow[i-1];
+//            }
+//            for (int j = 0; j < masRow.length; j++) {
+//                if (masRow[i] == masRow[j]) {
+//                    count++;
+//                }
+//            }
+//        }
+//        int countWordInLine = SamayaChastoPovtStroka.split(" ").length;
+//        System.out.println("Самая часто повторяющаяся строка - " + SamayaChastoPovtStroka);
+//        System.out.println("Количество слов в этой строке = " + countWordInLine);
+//    }
+
+    /**
+     * Решение через Map
+     */
+    public void stringArrayParsing(String[] arrString) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arrString.length; i++) {
+            if (map.containsKey(arrString[i])) {
+                map.put(arrString[i], map.get(arrString[i]) + 1);
+            } else {
+                map.put(arrString[i], 1);
+            }
+        }
+
+        int maxValueInMap=(Collections.max(map.values()));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == maxValueInMap) {
+                System.out.println("Самая часто повторяющаяся строка - " + entry.getKey());
+                System.out.println("Количество слов в этой сроке = " + entry.getKey().split(" ").length);
+            }
+        }
+    }
+}
